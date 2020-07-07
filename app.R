8eea3645a9e167a01d688baaf16f4099e4bf63d9
Bill Winnett <bwinnett12@gmail.com>
2020-07-07 17:42:28 -0500

    Tweaked UI and improved function

--- app.R
+++ app.R
@@ -1,83 +1,116 @@
-
-
-
 library(shiny)
 library(reticulate)
-library(system2)
+
+use_python("usr/bin/python")
 
 setwd("/home/bill/Projects/ncbifetcher/")
+source_python("fetcher.py")
 
 
-
-fun <- system('python //home//bill//Projects//ncbifetcher//test.py hello', 
-               intern = TRUE, wait=FALSE)
-
-# Define UI for app that draws a histogram ----
 ui <- fluidPage(
-
+  
   # App title ----
-  titlePanel("Hello Shiny!"),
-
-  # Sidebar layout with input and output definitions ----
+  titlePanel("An interesting title"),
+  
+  # Sidebar
   sidebarLayout(
-
-    # Sidebar panel for inputs ----
+    
+    # Sidebar panel (Email, Boolean operator, delete)
+    # Will include instructions
     sidebarPanel(
-
-      # Input: Slider for the number of bins ----
+      
+      # Email input
+      textInput(inputId = "email_input", 
+                label = "Email - Always tell NCBI who you are", value = ""),
+      
+      # Boolean operator to refine search
+      radioButtons("boolean", "Boolean Operator for search: ", c("And" = "AND",
+                                                                 "Or" = "OR")),
+      # Button for deleting file contents
+      actionButton("button_delete", "delete")
       
       
-      # Entrez email - required for parsing
-      textInput(inputId = "in_email", 
-                label = "Email - Always tell NCBI who you are", value = "")
-      
-      
-
-
     ),
-
-    # Main panel for displaying outputs ----
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    # Main panel for meat and potatoes of user input
     mainPanel(
-
-      # Output: ----
-      textOutput("test_output"),
       
-      selectInput(inputId = "input_type", label = "Type of query", choices = c(
-        "Species ID" = "id_species")),
+      # Species Input
+      textInput(inputId = "species_input", label = "Species: ", value = ""),
       
-      
-      textInput(inputId = "input_identifier", label = "", value = ""),
+      # Gene Input
+      textInput(inputId = "gene_input", label = "Gene: ", value = ""),
       
       actionButton("gobutton", "start"),
+    
       
-      textOutput("actionOutput")
+      textOutput("errorOutput"),
       
-
-
+      textOutput("testOutput")
+      
     )
   )
 )
 
 
-# Define server logic required to draw a histogram ----
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Server
 
 server <- function(input, output) {
-  
+    
+  errors_full <- ""
+  arguments <- ""
   
   observeEvent(input$gobutton, {
     
-    full_thing <- paste("python fetcher.py", input$input_identifier, sep=" ")
-
-    testing_python <- system(full_thing, 
-      intern=TRUE, wait=FALSE)
     
+    # Species
+    if (input$species_input == "") {
+      errors_full <- paste(errors_full, "missing species", sep=" ")
+      arguments <- paste(arguments, "species::null", sep=" ")
+    } else {
+      arguments <- paste(arguments, input$species_input, sep=" ")
+    }
     
-    output$actionOutput <-renderText(testing_python)
-
+    # Boolean Operator
+    arguments <- paste(arguments, input$boolean, sep=" ")
+    
+    # Gene
+    if (input$gene_input == "") {
+      errors_full <- paste(errors_full, "missing gene", sep=" ")
+      arguments <- paste(arguments, "gene::null", sep=" ")
+    } else {
+      arguments <- paste(arguments, input$gene_input, sep=" ")
+    }
+    
+    # Email
+    if (input$email_input == "") {
+      errors_full <- paste(errors_full, "missing email", sep=" ")
+    } 
+    
+    setwd("/home/bill/Projects/ncbifetcher/")
+    source_python("fetcher.py")
+    
+    # Reports how many files were fetched
+    python_output <- battery(arguments, input$email_input, "esame", ".")
+    
+    #Outputs errors
+    output$errorOutput <- renderText(errors_full)
+    
+    # An output for the ease of testing
+    output$testOutput <- renderText(python_output)
     
   })
+  
+  observeEvent(input$button_delete, {
+    delete_folder_contents()
+    
+  })
+  
 }
 
 
 shinyApp(ui, server)
- 
+
--- fetcher.py
+++ fetcher.py
@@ -1,8 +1,12 @@
 
-from Bio import Entrez, SeqIO
+__author__ = "Bill Winnett"
+__email__ = "bwinnett12@gmail.com"
+
+import glob
 import os
 import sys
-# import contextlib
+from Bio import Entrez
+
 
 # Debugging:
 # if its a xml -> Entrez.read(handle), if its text -> handle.read()
@@ -14,8 +18,8 @@ import sys
 # TODO - Add exceptions to not double download or release
 # TODO - Have a file checking method BEFORE parsing the gene bank
 
-
-def parse_ncbi(query):
+# REMINDER - Change emails
+def parse_ncbi(query, email):
     # Always tell ncbi who you are. Using mine until testing is over and the user will input theirs
     Entrez.email = "wwinnett@iastate.edu"
 
@@ -41,23 +45,25 @@ def parse_ncbi(query):
     return raw_data
 
 
-# TODO - Merge this into main app or just omit and have R shiny do thi
-# Only here for testing prior to R-shiny application
-# Gets information to the pass into main function
+# TODO - Something for user email
+# Gets information from R-shiny to then pass into main function
 def get_info():
-    organism_name = input("Organism name: ")
-    gene_name = input("Gene name: ")
-    option = input("And/or (default is And)")
+    organism_name = sys.argv[1]
+    gene_name = sys.argv[2]
+    option = sys.argv[3]
 
-    return organism_name + " OR " + gene_name if option.lower() == "or" else organism_name + " AND " + gene_name
+    # For the email. I'm just using mine for testing purposes
+    # email = sys.argv[4]
+
+    return " ".join([organism_name, option, gene_name])
 
 
 # TODO - Make this usable
 # TODO - Add comments
 # TODO - Turn it into a .fasta
 # Allows for the user to download the files as fasta
+# Only allows for one large File. Probably won't add individuals unless requested
 def to_fasta(raw_data, output_location):
-
     fasta_out = open(output_location, "w")
 
     for i in range(int(len(raw_data))):
@@ -76,8 +82,9 @@ def to_fasta(raw_data, output_location):
 # TODO - Add exceptions in case it doesn't work
 def to_gb(raw_data, output_folder):
 
+    files_downloaded = []
     # Creates a temp file and saves the parsed (ncbi) data to. If no temp file, creates one
-    # I know this is extra work, I'm just not sure how to do it cleaner
+    # I know this is extra work, but I'm just not sure how to do it cleaner
     try:
         temp_file = open("./temp.txt", "x")
     except FileExistsError as e:
@@ -103,8 +110,10 @@ def to_gb(raw_data, output_folder):
             split = line.split()
             gb_location = output_folder + split[1] + ".gb"
 
+            # If there is no file, creates one and then adds the name for log outputting
             if not os.path.isfile(gb_location):
                 current_file = open(gb_location, "x")
+                files_downloaded.append(split[1])
 
             current_file = open(gb_location, "w")
 
@@ -115,25 +124,58 @@ def to_gb(raw_data, output_folder):
         temp_file.close()
         os.remove("./temp.txt")
     except FileNotFoundError as e:
-        print(e)
+        pass
+
+    return str(len(files_downloaded)) + " files downloaded. Names are: " + str(files_downloaded)
+
+
+def battery(search, email, method, output_location):
+
+    print("Got to battery")
+    output_location = "./output/"
+    method = "esame"
+    return_to_r = ""
+
+    if method == "gb" or method == "esame":
+        print("Got to to_gb")
+        return_to_r = to_gb(parse_ncbi(search, email), output_location)
+
+    elif method == "fasta":
+        # TODO - make this return something
+        return_to_r = to_fasta(parse_ncbi(search, email), output_location)
+    else:
+        return_to_r = "Failed"
+        print("Failed")
+        return "Failed"
+
+    print("exited function")
+    return return_to_r
+
+
+def delete_folder_contents():
+    # files = glob.glob("./output/")
+    # for f in files:
+    #     os.remove(f)
+
+    for file in os.scandir("./output/"):
+        if file.name.endswith(".gb"):
+            os.unlink(file.path)
 
 
 def main():
-    # test_identifier = "Opuntia AND rpl16"
-    test_identifier = sys.argv[1]
-    test_output_folder = "./output/"
+    test_identifier = "Opuntia AND rpl16"
+    r = 2
+    # test_output_folder = "./output/"
+    # print(battery(test_identifier, " ", "esame", " "))
 
-    test_parse = parse_ncbi(test_identifier)
-
-    # to_fasta(test_parse, test_output)
-    to_gb(test_parse, test_output_folder)
-
-    # Gve it a taxonomic id 9606
-    # Download gene bank files for a taxonomic id
+    # delete_folder_contents()
 
 
 if __name__ == "__main__":
     main()
+
+
+
 
 # Commented out section
 # dict_keys(['GBSeq_locus', 'GBSeq_length', 'GBSeq_strandedness', 'GBSeq_moltype',
