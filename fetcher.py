2c884668c60fe17d5c9a2ca22e9b70168a568241
Bill Winnett <bwinnett12@gmail.com>
2020-08-22 23:24:29 -0500

    Fixed errors

--- fetcher.py
+++ fetcher.py
@@ -19,7 +19,6 @@ def parse_ncbi(query_from_user, output_type, out_loc):
     # Records those who match the query and then formats them so it can fetch them
     record = Entrez.read(handle)
     gi_query = ",".join(record["IdList"])
-    print(gi_query)
 
     retmode_input = ("text", "xml")[output_type == "fasta"]
 
@@ -38,7 +37,6 @@ def parse_ncbi(query_from_user, output_type, out_loc):
 
 # Creates folders for each of the accession numbers
 def create_folders(id_list, out_folder):
-    print(id_list)
     for entry in range(len(id_list)):
         if not os.path.isdir(out_folder + id_list[entry]):
             os.makedirs(out_folder + id_list[entry])
@@ -52,7 +50,6 @@ def delete_folder_contents(folder_loc):
         if filename.endswith(".gb") or filename.endswith(".fa") or filename.endswith(".faa") or os.path.isdir(filename):
             num_deleted += 1
             # os.unlink(file.path)
-            print(filename)
 
     return "Files Deleted: %d" % num_deleted
 
@@ -60,27 +57,8 @@ def delete_folder_contents(folder_loc):
 # Something to run to run both functions. Ultimately will be done using R (Front End)
 def battery(search_query, output_folder):
 
-    table = {
-        'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
-        'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
-        'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
-        'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
-        'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
-        'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
-        'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
-        'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
-        'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
-        'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
-        'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
-        'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
-        'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
-        'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
-        'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
-        'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W',
-    }
 
-
-    return_to_r = write_to_fasta(parse_ncbi(search_query, "fasta", output_folder), output_folder, table)
+    return_to_r = write_to_fasta(parse_ncbi(search_query, "fasta", output_folder), output_folder)
     return_to_r += "\n" + write_to_gb(parse_ncbi(search_query, "text", output_folder), output_folder)
 
     lst = ["species", "genes"]
