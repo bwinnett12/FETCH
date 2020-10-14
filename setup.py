02d05a66cf9d4cf174cff1977b2d61bc293eb3e4
Bill Winnett <bwinnett12@gmail.com>
2020-10-14 18:53:22 -0500

    'needing a setup' why not have every package already installed smh my head

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(name='ncbifetcher',
+      version='0.3.0',
+      description='Fetch ncbi and pull to any location',
+      url='https://github.com/bwinnett12/ncbifetcher',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+      ],
+      author='Bill Winnett',
+      author_email='wwinnett@iastate.edu',
+      license='MIT',
+      # packages=['ncbifetch'],
+      install_requires=[
+            'markdown',
+            'biopython',
+            'glob3',
+            'argparse',
+      ],
+      # entry_points = {
+      #   'console_scripts': ['ncbifetch=ncbifetch.command_line:main'],
+      # },
+      include_package_data=True,
+      zip_safe=False)
