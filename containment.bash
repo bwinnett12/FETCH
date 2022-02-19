85faee1b63bc6ffd919658962853cfbc75df7fcb
Bill Winnett <bwinnett12@gmail.com>
2022-02-18 19:01:38 -0600

    Added contained dependencies

---
+++ containment.bash
@@ -0,0 +1,5 @@
+
+## This is a file to set up the dependencies without installing them to the system. Useful if you are working on a larger shared system (HPC, cluster, etc....)
+
+pip install -r requirements.txt -t src/
+
